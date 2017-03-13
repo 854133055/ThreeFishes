9babe6c3c06073a486e163735ca6aea276c6f63d
854133055 <854133055@qq.com>
2017-03-13 16:34:39 +0800

    toolbar部分完成

--- app/src/main/java/com/android_threefishes/threefish/a3fish/MainActivity.java
+++ app/src/main/java/com/android_threefishes/threefish/a3fish/MainActivity.java
@@ -1,10 +1,18 @@
 package com.android_threefishes.threefish.a3fish;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.balysv.materialmenu.MaterialMenuDrawable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +23,18 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private List<Fragment> fragmentList;
     private TabLayout.Tab myTab;
+    private Toolbar mbar;
+    private TextView textView;
+    private MaterialMenuDrawable materialMenuDrawable;
+    private MaterialMenuDrawable.AnimationState mAnimationState;
+    private int iconStates = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
         initView();
     }
 
@@ -32,14 +48,52 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    private List<Fragment> initFragmentlist(){
-        fragmentList = new ArrayList<Fragment>();
-        fragmentList.add(new Find_Fragment());
-        fragmentList.add(new Selection_Fragment());
-        fragmentList.add(new My_Fragment());
-        return fragmentList;
+    private void initToolbar(){
+        textView = (TextView) findViewById(R.id.title_text);
+        mbar = (Toolbar) findViewById(R.id.toolbar);
+        mbar.setTitle("");
+        mbar.inflateMenu(R.menu.searchbtn);
+        toolbarIconEvent();
+        mbar.setNavigationIcon(materialMenuDrawable);
+        mbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.search_btn){
+                    Toast.makeText(MainActivity.this,"搜素图标",Toast.LENGTH_LONG).show();
+                }
+                return true;
+            }
+        });
     }
+
+    /**
+     * NavigationIcon的变化动画与处理,存疑?
+     */
+    private void toolbarIconEvent(){
+        materialMenuDrawable = new MaterialMenuDrawable
+                (this, Color.BLACK, MaterialMenuDrawable.Stroke.THIN);
+        //mAnimationState = MaterialMenuDrawable.AnimationState.BURGER_ARROW;
+        materialMenuDrawable.animateIconState(MaterialMenuDrawable.IconState.BURGER);
+        mbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //      materialMenuDrawable.setTransformationOffset(mAnimationState, 1);
+                switch (iconStates){
+                    case 1:
+                        materialMenuDrawable.animateIconState(MaterialMenuDrawable.IconState.BURGER);
+                        iconStates = 0;
+                        break;
+                    case 0:
+                        materialMenuDrawable.animateIconState(MaterialMenuDrawable.IconState.ARROW);
+                        iconStates = 1;
+                        break;
+                }
+            }
+        });
+
+    }
+
+
 
     /**
      * for循环设定tab的icon,name
@@ -75,6 +129,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private List<Fragment> initFragmentlist(){
+        fragmentList = new ArrayList<Fragment>();
+        fragmentList.add(new Find_Fragment());
+        fragmentList.add(new Selection_Fragment());
+        fragmentList.add(new My_Fragment());
+        return fragmentList;
     }
 
     /**
